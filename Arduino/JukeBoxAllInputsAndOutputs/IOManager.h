16b4045fc762f72d28171b178a2228eee6eb8f79
Imanol Gomez <yo@imanolgomez.net>
2020-01-23 13:29:51 +0100

    improved serial reading

--- Arduino/JukeBoxAllInputsAndOutputs/IOManager.h
+++ Arduino/JukeBoxAllInputsAndOutputs/IOManager.h
@@ -118,8 +118,8 @@ void IOManager::setup()
     Serial.println("IOManager::setup");
     initializeIO();
     initializeButtons();
-    //initializeRegisters();
-    //initializeOutputs();
+    initializeRegisters();
+    initializeOutputs();
    
 }
 
@@ -190,13 +190,16 @@ void IOManager::initializeOutputs()
     Serial.println("IOManager::initializeOutputs!"); 
     int offset = 15 - NUM_OUTPUTS;
     for(int i =0 ; i< NUM_OUTPUTS; i++){
-      outputs[i] = Output(&io1, offset+i); 
+      outputs[i] = Output(&io1, offset+i+1*i); 
       Serial.print("IOManager::added output ");  
       Serial.print(i);  
       Serial.print(", output: ");  
-      Serial.print(offset+i); 
+      Serial.print(offset+i+1*i); 
       Serial.println(" to io1"); 
     }
+
+    this->setOutput(0, 1);
+    this->setOutput(1, 0);
 }
 
 
@@ -215,8 +218,8 @@ void IOManager::setOutput(uint8_t id, bool value)
 void IOManager::update()
 {   
     updateButtons();
-    //updateRegisters();
-    //updateOutputs();   
+    updateRegisters();
+    updateOutputs();   
 }
 
 
--- OpenFrameworks/JukeboxPlayerApp/src/I0/SerialManager.cpp
+++ OpenFrameworks/JukeboxPlayerApp/src/I0/SerialManager.cpp
@@ -288,21 +288,50 @@ void SerialManager::update()
         return;
     }
     
-    int numBytes = m_serial.available();
-    
-    if ( numBytes == 0 )
+    // we want to read 7 bytes
+    int bytesRequired = HEADER_SIZE + DATA_SIZE;
+    unsigned char bytes[bytesRequired];
+    int bytesRemaining = bytesRequired;
+
+    // loop until we've read everything
+    while ( bytesRemaining > 0 )
     {
-        return;
+        // check for data
+        if ( m_serial.available() > 0 )
+        {
+            // try to read - note offset into the bytes[] array, this is so
+            // that we don't overwrite the bytes we already have
+            int bytesArrayOffset = bytesRequired - bytesRemaining;
+            int result = m_serial.readBytes( &bytes[bytesArrayOffset],
+            bytesRemaining );
+        
+            // check for error code
+            if ( result == OF_SERIAL_ERROR )
+            {
+                // something bad happened
+                ofLog( OF_LOG_ERROR, "unrecoverable error reading from serial" );
+                // bail out
+            break;
+            }
+            else if ( result == OF_SERIAL_NO_DATA )
+            {
+                // nothing was read, try again
+            }
+            else
+            {
+                // we read some data!
+                bytesRemaining -= result;
+            }
+        }
     }
-    
-    
-    unsigned char bytes[numBytes];
-    
-    int result = m_serial.readBytes( bytes, numBytes );
-    
+
+
+
     string str((char *)bytes);
     
     ofLogNotice() <<"SerialManager::update << Received: " << str;
+    ofLogNotice() <<"SerialManager::update << Num Bytes Received: " << bytesRemaining;
+    
     
     // check for error code
     if ( result == OF_SERIAL_ERROR ){
@@ -314,7 +343,7 @@ void SerialManager::update()
     }
     
     
-    this->parseData(bytes, numBytes);
+    this->parseData(bytes, bytesRequired);
 }
 
 
